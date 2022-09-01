4d7854be4a26db87b73c29bbdd71edb58e6b62dd
taeju-moon <pp2lycee@naver.com>
2022-09-01 12:18:14 +0900

    test for deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-function App() {
+export default function App() {
+  const [forms, setForms] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/forms`)
+      .then(({ data: { data } }) => setForms(data.forms))
+      .catch((error) => console.log(error));
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {forms.map((form) => (
+        <div style={{ marginBottom: 20 }}>
+          <p>제목: {form.title}</p>
+          <p>내용: {form.content} </p>
+          <p>카테고리: {form.category}</p>
+        </div>
+      ))}
+    </>
   );
 }
-
-export default App;
